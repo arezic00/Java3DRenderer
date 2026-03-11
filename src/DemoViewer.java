8793526c1789980812ea9d1b9fff9e0d9a8dda0a
Antonio Rezic <antonio.rezic00@gmail.com>
2026-03-11 12:02:43 +0100

    Draw tetrahedron in orthographic projection

--- src/DemoViewer.java
+++ src/DemoViewer.java
@@ -1,5 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Path2D;
+import java.util.List;
+import java.util.ArrayList;
 
 public class DemoViewer {
     public static void main(String[] args) {
@@ -21,13 +24,40 @@ public class DemoViewer {
             public void paintComponent(Graphics g) {
                 Graphics2D g2 = (Graphics2D) g;
                 g2.setColor(Color.BLACK);
-                g2.fillRect(0,0, pane.getWidth(), pane.getHeight());
+                g2.fillRect(0,0, getWidth(), getHeight());
+
+                //A tetrahedron
+                List<Triangle> triangles = new ArrayList<>();
+                triangles.add(new Triangle(new Vertex(100, 100, 100),
+                        new Vertex(-100, -100, 100),
+                        new Vertex(-100, 100, -100),
+                        Color.WHITE));
+                triangles.add(new Triangle(new Vertex(100, 100, 100),
+                        new Vertex(-100, -100, 100),
+                        new Vertex(100, -100, -100),
+                        Color.RED));
+                triangles.add(new Triangle(new Vertex(-100, 100, -100),
+                        new Vertex(100, -100, -100),
+                        new Vertex(100, 100, 100),
+                        Color.GREEN));
+                triangles.add(new Triangle(new Vertex(-100, 100, -100),
+                        new Vertex(100, -100, -100),
+                        new Vertex(-100, -100, 100),
+                        Color.BLUE));
+
+                g2.translate(getWidth() / 2, getHeight() / 2);
+                g2.setColor(Color.WHITE);
+                for (Triangle t : triangles) {
+                    Path2D path = new Path2D.Double();
+                    path.moveTo(t.v1.x, t.v1.y);
+                    path.lineTo(t.v2.x, t.v2.y);
+                    path.lineTo(t.v3.x, t.v3.y);
+                    path.closePath();
+                    g2.draw(path);
+                }
             }
         };
         pane.add(renderPanel, BorderLayout.CENTER);
-
-
-
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 400);
