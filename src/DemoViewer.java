cc306376ed133372549d7b823e02f9a61e8a766b
Antonio Rezic <antonio.rezic00@gmail.com>
2026-03-19 20:52:51 +0100

    Add inflating for rounded shapes

--- src/DemoViewer.java
+++ src/DemoViewer.java
@@ -46,6 +46,11 @@ public class DemoViewer {
                         new Vertex(-100, -100, 100),
                         Color.BLUE));
 
+                //Rounding by inflating
+                for (int i = 0; i <= 5; i++) {
+                    triangles = inflate(triangles);
+                }
+
                 double yRotation = Math.toRadians(horizontalSlider.getValue());
                 Matrix3 yRotationMatrix = new Matrix3(new double[] {
                    Math.cos(yRotation), 0, -Math.sin(yRotation),
@@ -154,5 +159,30 @@ public class DemoViewer {
         int blue = (int) Math.pow(blueLinear, 1/2.4);
 
         return new Color(red, green, blue);
+    }
+
+    public static List<Triangle> inflate(List<Triangle> tris) {
+        List<Triangle> result = new ArrayList<>();
+        for (Triangle t : tris) {
+            Vertex m1 =
+                    new Vertex((t.v1.x + t.v2.x)/2, (t.v1.y + t.v2.y)/2, (t.v1.z + t.v2.z)/2);
+            Vertex m2 =
+                    new Vertex((t.v2.x + t.v3.x)/2, (t.v2.y + t.v3.y)/2, (t.v2.z + t.v3.z)/2);
+            Vertex m3 =
+                    new Vertex((t.v1.x + t.v3.x)/2, (t.v1.y + t.v3.y)/2, (t.v1.z + t.v3.z)/2);
+            result.add(new Triangle(t.v1, m1, m3, t.color));
+            result.add(new Triangle(t.v2, m1, m2, t.color));
+            result.add(new Triangle(t.v3, m2, m3, t.color));
+            result.add(new Triangle(m1, m2, m3, t.color));
+        }
+        for (Triangle t : result) {
+            for (Vertex v : new Vertex[] { t.v1, t.v2, t.v3 }) {
+                double l = Math.sqrt(v.x * v.x + v.y * v.y + v.z * v.z) / Math.sqrt(30000);
+                v.x /= l;
+                v.y /= l;
+                v.z /= l;
+            }
+        }
+        return result;
     }
 }
